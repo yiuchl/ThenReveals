8d1c30529eccdc3a5b52eab495a5c09e1788c338
chloe yiu <chloeyiu@icloud.com>
2024-03-05 17:03:29 -0500

    Update divs and fixed the footer

---
+++ PLAYGROUND/play.js
@@ -0,0 +1,27 @@
+// /* Draggable divs */
+// $(function () {
+// 	$("#div1").draggable({
+// 		containment: "document",
+// 	});
+
+// 	$("#div2").draggable({
+// 		containment: "document",
+// 	});
+// 	$("#div3").draggable({
+// 		containment: "document",
+// 	});
+
+// 	$("#div4").draggable({
+// 		containment: "document",
+// 	});
+// 	$("#div5").draggable({
+// 		containment: "document",
+// 	});
+
+// 	$("#div6").draggable({
+// 		containment: "document",
+// 	});
+// 	$("#div7").draggable({
+// 		containment: "document",
+// 	});
+// });
